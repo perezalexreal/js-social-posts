568b9df15d22158b31eb19377a059d133b4b1f0d
perezalexreal <perezalexreal@gmail.com>
2022-01-11 16:16:24 +0100

    Caricamento Dati

---
+++ main.js
@@ -0,0 +1,22 @@
+const dataPost = [
+    {
+        id: 1,
+        nome: '',
+        foto: '',
+        data: '',
+        testo: '',
+        immagine: '',
+        likes: 0
+    },
+    {
+        id: 2,
+        nome: '',
+        foto: '',
+        data: '',
+        testo: '',
+        immagine: '',
+        likes: 0
+    }
+
+]
+
